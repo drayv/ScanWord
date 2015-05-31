562c8893d42b3e8e9c3497066ee071020db9b79f
drayv <drayv.mj12@gmail.com>
2015-05-31 18:10:37 +0300

    add bootstrap + jquery, and start work on "add material"

---
+++ WatchWord.Web.UI/Controllers/MaterialsController.cs
@@ -0,0 +1,51 @@
+﻿namespace WatchWord.Web.UI.Controllers
+{
+    using System.IO;
+    using System.Web.Mvc;
+
+    using WatchWord.Web.UI.Models.Materials;
+
+    /// <summary>
+    /// The materials controller.
+    /// </summary>
+    public class MaterialsController : Controller
+    {
+        /// <summary>
+        /// Creates a form for adding a new material to the site.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult Add()
+        {
+            var addViewModel = new AddViewModel();
+            return this.View(addViewModel);
+        }
+
+        /// <summary>
+        /// HttpPost of the AddViewModel.
+        /// </summary>
+        /// <param name="model">
+        /// The AddViewModel.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        [HttpPost]
+        public ActionResult Add(AddViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            using (var memoryStream = new MemoryStream())
+            {
+                model.File.InputStream.CopyTo(memoryStream);
+            }
+
+            // TODO: Redirect to the material page.
+            return this.View();
+        }
+    }
+}
---
+++ WatchWord.Web.UI/Models/Materials/AddViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace WatchWord.Web.UI.Models.Materials
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Web;
+
+    /// <summary>
+    /// The add view model.
+    /// </summary>
+    public class AddViewModel
+    {
+        /// <summary>
+        /// Gets or sets the file.
+        /// </summary>
+        [Required(ErrorMessage = "Please select the file!")]
+        [FileExtensions(Extensions = "txt,srt", ErrorMessage = "Please upload valid format!")]
+        public HttpPostedFileBase File { get; set; }
+    }
+}
